80fe583bad0a703ae1c6cd56e08604a39309ee16
remyagrv <remyajose.mec@gmail.com>
2022-09-01 16:57:00 +0530

    clone

--- js/app.js
+++ js/app.js
@@ -82,11 +82,15 @@ async function activateXR() {
   );
 
   let customModel;
+  let clone;
+  if(clone){
+    scene.remove(clone);
+  }
   // Eventlistener that will wait for a 'tap' of the user. When tapped, custom model loads with animation and get placed on marker location
   session.addEventListener("select", () => {
     loader.load("models/Demon/Demon.gltf", function (gltf) {
       customModel = gltf.scene;
-
+      clone = customModel;
       // Scale of model
       customModel.scale.multiplyScalar(8);
 
@@ -105,6 +109,7 @@ async function activateXR() {
       action.play();
 
       scene.add(customModel);
+      
     });
   });
 
