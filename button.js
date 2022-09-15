4ed027ad2e83572e0d98af406a68d95af507a1ab
remyagrv <remyajose.mec@gmail.com>
2022-09-15 14:55:48 +0530

    nn

--- button.js
+++ button.js
@@ -1,19 +1,21 @@
-
-
 ARbtn.style.display = "none";
 // document.getElementById("ARbtn").addEventListener("click", function() {
-    if(navigator.userAgent.match(/Android/i)){
-        ARbtn.style.display = "block";
-        console.log("Button visible only on Android devices");
-        } 
-        else if(navigator.userAgent.match(/iPhone/i)){
-        ARbtn.style.display = "none";
-        console.log("AR button not visible in iPhone");
-        
-       } 
-       else if(navigator.userAgent.match(/Windows/i)){
-        
-        ARbtn.style.display = "none";
-        console.log("AR button not visible in Windows");
-    }
-//   });
+if (navigator.userAgent.match(/Android/i)) {
+  ARbtn.style.display = "block";
+  console.log("Button visible only on Android devices");
+} else if (navigator.userAgent.match(/iPhone/i)) {
+  ARbtn.style.display = "none";
+  console.log("AR button not visible in iPhone");
+} else if (navigator.userAgent.match(/Windows/i)) {
+  ARbtn.style.display = "none";
+  console.log("AR button not visible in Windows");
+}
+else if (navigator.userAgent.match(/iPad/i)) {
+  ARbtn.style.display = "none";
+  console.log("AR button not visible in iPad");
+} else if (navigator.userAgent.match(/iPod/i)) {
+  ARbtn.style.display = "none";
+  console.log("AR button not visible in iPod");
+}else{
+    ARbtn.style.display = "none";
+}
